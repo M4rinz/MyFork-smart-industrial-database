b1ddb239804329176008119a0f55d1277bfe1a86
DadeOrsu <dade.orsu@gmail.com>
2024-11-24 19:10:02 +0100

    Cleansing of the repository. Removed all the files that are not needed anymore. And renamed the filling.py file to query_template.py

--- backup.py
+++ backup.py
@@ -3,27 +3,28 @@ import subprocess
 from datetime import datetime
 from cryptography.fernet import Fernet
 
+
 def load_key():
-    with open("backups_decryption\\enc_key.key", "rb") as key_file:
+    with open("backups_decryption/enc_key.key", "rb") as key_file:
         return key_file.read()
 
-def encrypt_backup_file(filename,file_data):
+
+def encrypt_backup_file(filename, file_data):
     """Encrypt the content of a file."""
     key = load_key()
     fernet = Fernet(key)
-    
+
     # Encrypt the file data
     encrypted_data = fernet.encrypt(file_data)
-    
     # Write the encrypted data back to the file
     with open(filename, "wb") as file:
         file.write(encrypted_data)
     print(f"File '{filename}' has been encrypted.")
 
+
 def backup_postgres_database(db_name, user, password, host='localhost', port=5432, backup_dir='backups'):
     """
     Backs up a PostgreSQL database using pg_dump
-    
     Parameters:
         db_name (str): The name of the database to back up
         user (str): The PostgreSQL username
@@ -36,15 +37,12 @@ def backup_postgres_database(db_name, user, password, host='localhost', port=543
         # Create the backup directory if it doesn't exist
         if not os.path.exists(backup_dir):
             os.makedirs(backup_dir)
-        
         # Generate a timestamped filename
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         backup_file = os.path.join(backup_dir, f"{db_name}_backup_{timestamp}.sql")
-        
         # Set the PGPASSWORD environment variable for authentication
         env = os.environ.copy()
         env["PGPASSWORD"] = password
-        
         dump_command = [
             "pg_dump",
             "-h", host,
@@ -53,24 +51,20 @@ def backup_postgres_database(db_name, user, password, host='localhost', port=543
             "-F", "c",  # Custom format; for plain SQL, use '-F p'
             db_name
         ]
-        
         # Execute the command and capture the output
         result = subprocess.run(dump_command, env=env, check=True, stdout=subprocess.PIPE)
-        
         # Return the output as a byte stream
         encrypt_backup_file(backup_file,result.stdout)
-
-        
         print(f"Backup successful! File saved to: {backup_file}")
-    
     except Exception as e:
         print(f"An error occurred during the backup: {e}")
 
+
 # Configuration
-DB_NAME = "dbname"
-DB_USER = "username"
+DB_NAME = "KPI_database"
+DB_USER = "postgres"
 DB_PASSWORD = "password"
-DB_HOST = "localhost"
+DB_HOST = "172.17.0.2"
 DB_PORT = 5432
 BACKUP_DIR = "backups_encrypted"
 
--- backups_decryption/decrypt_backup.py
+++ backups_decryption/decrypt_backup.py
@@ -1,12 +1,14 @@
 from cryptography.fernet import Fernet
 
+
 def load_key():
     with open("enc_key.key", "rb") as key_file:
         return key_file.read()
 
-def decrypt(filepath,key):
+
+def decrypt(filepath, key):
     data = None
-    with open(filepath,"rb") as f_backup:
+    with open(filepath, "rb") as f_backup:
         data = f_backup.read()
     if (data is None) or (len(data) <= 0):
         return False 
@@ -14,8 +16,8 @@ def decrypt(filepath,key):
     decrypted_data = fernet.decrypt(data)
     filename = filepath.split("/")[-1]
     print(filename)
-    with open("decrypted_"+filename,"wb") as f_backup:
+    with open("decrypted_" + filename, "wb") as f_backup:
         f_backup.write(decrypted_data)
-    
+
 
 decrypt("../backups/SmartApps_backup_20241121105637.sql",load_key())
